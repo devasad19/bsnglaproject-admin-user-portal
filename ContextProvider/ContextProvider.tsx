360067a1a247c26a234a1e1e7240f7df80449898
razibul <razibul15-11649@diu.edu.bd>
2024-09-14 12:59:56 +0600

    context api user condition added

--- ContextProvider/ContextProvider.tsx
+++ ContextProvider/ContextProvider.tsx
@@ -1,25 +1,21 @@
-"use client"
-import { TUser } from '@/types/user/User';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { cookies } from 'next/headers';
-import React, { useEffect, useState } from 'react'
-import { createContext } from 'react'
+"use client";
+import { TUser } from "@/types/user/User";
+import axios from "axios";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { createContext } from "react";
 
-type TContextType ={
-  
-  setFeatureSelectedInfoAll?:any;
-  featureSelectedInfoAll?:any;
-  featureTotalPrice?:any;
-  setFeatureTotalPrice?:any;
-  user:TUser | {}
-
-}
-
+type TContextType = {
+  setFeatureSelectedInfoAll?: any;
+  featureSelectedInfoAll?: any;
+  featureTotalPrice?: any;
+  setFeatureTotalPrice?: any;
+  user: TUser | {};
+};
 
 export const MyContext = createContext<TContextType | undefined>(undefined);
 
- const ContextProvider = ({children}:any)=> {
+const ContextProvider = ({ children }: any) => {
   const [featureSelectedInfoAll, setFeatureSelectedInfoAll] = useState<any>([]);
   const [featureTotalPrice, setFeatureTotalPrice] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
@@ -28,37 +24,41 @@ export const MyContext = createContext<TContextType | undefined>(undefined);
   useEffect(() => {
     setLoading(true);
     const token = Cookies.get("token");
-    const user:string | undefined =  Cookies.get("user");
-    const userInfo = JSON.parse(user ?? "");
+    const user: string | undefined = Cookies.get("user");
+    let userInfo;
+    if (user) {
+      userInfo = JSON?.parse(user ?? "");
+    }
+    // const userInfo = JSON?.parse(user ?? "");
     if (token && userInfo) {
-      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/details/${userInfo.id}`,{
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        console.log("GLobal Context :",res.data.data);
-        setUser(res.data.data);
-        setLoading(false);
-      }).catch((err) => {
-        setLoading(false);
-        // console.log(err);
-      })
+      axios
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/user/details/${userInfo.id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          console.log("GLobal Context :", res.data.data);
+          setUser(res.data.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+          // console.log(err);
+        });
     }
   }, []);
 
-    const contextValue:TContextType ={
-        setFeatureSelectedInfoAll,
-        featureSelectedInfoAll,
-        setFeatureTotalPrice,
-        featureTotalPrice,
-        user
-    }
+  const contextValue: TContextType = {
+    setFeatureSelectedInfoAll,
+    featureSelectedInfoAll,
+    setFeatureTotalPrice,
+    featureTotalPrice,
+    user,
+  };
   return (
-    <MyContext.Provider value={contextValue}>
-      {children}
-    </MyContext.Provider>
-  )
-}
+    <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
+  );
+};
 
-export default ContextProvider
+export default ContextProvider;
