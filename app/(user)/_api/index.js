a95654bef6b8641426b71e844548c6fd397978f0
Sadaf <monower.mysoftheaven@gmail.com>
2024-10-30 15:00:55 +0600

    added purchased service details api

--- app/(user)/_api/index.js
+++ app/(user)/_api/index.js
@@ -5,6 +5,24 @@ import Cookies from "js-cookie";
 const token = Cookies.get("token");
 
 
+export const getServicePurchaseHistory = async (id) =>{
+    try {
+        const data = await axios
+            .get(`${process.env.NEXT_PUBLIC_API_URL}/user/service/purchase-list/${id}`)
+            .then((res) => {
+                return res?.data;
+            })
+            .catch((err) => {
+                console.log(err);
+                return err;
+            });
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
 export const getInvoiceDetails = async (id)=>{
     try {
         const data = await axios
--- app/(user)/user/accounts-settings/purchase-services/[id]/page.jsx
+++ app/(user)/user/accounts-settings/purchase-services/[id]/page.jsx
@@ -1,63 +1,106 @@
+'use client';
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { getServicePurchaseHistory } from "@/app/(user)/_api";
 
 
-const Home = async ({params : { id }}) => {
-    return (
-      <section>
-        <div className="flex items-center gap-4 pb-5">
-          <Link
-            href={{
-              pathname: "/user/accounts-settings/purchase-services",
-            }}
-            shallow
-          >
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+const Home = ({ params: { id } }) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getServicePurchaseHistory(12)
+      .then((response) => {
+        setData(response?.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // console.log('payment history response: ', data);
+
+
+  return (
+    <>
+      {
+        loading ? (
+          <p>Loading...</p>
+        ) : (
+          <section>
+            <div className="flex items-center gap-4 pb-5">
+              <Link
+                href={{
+                  pathname: "/user/accounts-settings/purchase-services",
+                }}
+                shallow
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                />
-              </svg>
-            </span>
-          </Link>
-          <h3 className="text-26 lg:text-32 font-mono font-bold text-[#151D48]">
-            Service Name: 
-          </h3>
-        </div>
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                    />
+                  </svg>
+                </span>
+              </Link>
+              <h3 className="text-26 lg:text-32 font-mono font-bold text-[#151D48]">
+                Service Name:
+              </h3>
+            </div>
 
-        <h3 className="text-28 font-bold text-[#151D48] pb-5">
+            {/* <h3 className="text-28 font-bold text-[#151D48] pb-5">
           Citizen Name: Md. Shakhawat Hossain
-        </h3>
-        <div className="overflow-x-auto bg-white rounded shadow-lg mb-5">
-          <table className="w-full lg:table-fixed text-center">
-            <thead className="h-10 bg-blue-500 text-white">
-              <tr>
-                <th>Feature Name</th>
-                <th>Purchasing Date</th>
-                <th>Valid Till</th>
-                <th>Remain Days</th>
-                <th>Price</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="h-12">
-                <td>OCR</td>
-                <td>2022-01-01</td>
-                <td>2022-01-01</td>
-                <td>60 days</td>
-                <td>1000</td>
-                <td>Active</td>
-              </tr>
-              <tr className="h-12 border-t border-black">
+        </h3> */}
+            <div className="overflow-x-auto bg-white rounded shadow-lg mb-5">
+              <table className="w-full lg:table-fixed text-center">
+                <thead className="h-10 bg-blue-500 text-white">
+                  <tr>
+                    <th>Feature Name</th>
+                    <th>Purchasing Date</th>
+                    <th>Valid Till</th>
+                    <th>Remain Days</th>
+                    <th>Price</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    data?.map((item, index) => {
+                      return (
+                        <tr key={index} className="h-12">
+                          <td>
+                            {
+                              item?.feature?.name
+                            }
+                          </td>
+                          <td>
+                            {
+                              item?.created_at?.slice(0,9)
+                            }
+                          </td>
+                          <td>NA</td>
+                          <td>NA</td>
+                          <td>
+                            {
+                              item?.total
+                            }
+                          </td>
+                          <td>NA</td>
+                        </tr>
+                      )
+                    })
+                  }
+
+                  {/* <tr className="h-12 border-t border-black">
                 <td>Validity</td>
                 <td>2022-01-01</td>
                 <td>2022-01-01</td>
@@ -72,13 +115,13 @@ const Home = async ({params : { id }}) => {
                 <td>60 days</td>
                 <td>1000</td>
                 <td>Active</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
- 
-     
-        <h3 className="text-28 font-bold text-[#151D48] pb-5">
+              </tr> */}
+                </tbody>
+              </table>
+            </div>
+
+
+            {/* <h3 className="text-28 font-bold text-[#151D48] pb-5">
           Citizen Name: Md. Jobbar Hossain
         </h3>
         <div className="overflow-x-auto bg-white rounded shadow-lg mb-5">
@@ -104,9 +147,12 @@ const Home = async ({params : { id }}) => {
               </tr>
             </tbody>
           </table>
-        </div>
-      </section>
-    );
+        </div> */}
+          </section>
+        )
+      }
+    </>
+  );
 };
 
 export default Home;
