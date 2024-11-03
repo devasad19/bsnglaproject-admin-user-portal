2d83cfeb4bd0e856b326597f6cdeb5951b7e69df
Sadaf <monower.mysoftheaven@gmail.com>
2024-11-03 10:43:17 +0600

    minor changes

--- app/(admin)/admin/services/page.jsx
+++ app/(admin)/admin/services/page.jsx
@@ -125,119 +125,127 @@ const Home = () => {
               )}
               {services?.length > 0
                 ? services?.map((item, index) => {
-                    const paidStatus = JSON?.parse(item?.paid_status || "{}");
-                    return (
-                      <tr key={index}>
-                        <td className="px-2 border-r border-gray-200">
-                          <Link
-                            href={{
-                              pathname: `/admin/services/${item?.id}`,
-                            }}
-                            className="flex items-center gap-2 text-14"
-                            shallow
-                          >
-                            <span className="flex items-center gap-3">
-                              <Image
-                                className="w-10 h-10 rounded-md"
-                                src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${item?.logo}`}
-                                height={1000}
-                                width={1000}
-                                alt="Bangla"
-                              />
-                              {item?.name || " "}
-                            </span>
-                          </Link>
-                        </td>
-                        <td className="text-center border-r border-gray-200">
-                          {item?.sub_title?.length > 35
-                            ? item?.sub_title?.substring(0, 25) + "..."
-                            : item?.sub_title}
-                        </td>
-                        <td className="text-center border-r border-gray-200">
-                          {item?.type || ""}
-                        </td>
+                  const paidStatus = JSON?.parse(item?.paid_status || "{}");
+                  return (
+                    <tr key={index}>
+                      <td className="px-2 border-r border-gray-200">
+                        <Link
+                          href={{
+                            pathname: `/admin/services/${item?.id}`,
+                          }}
+                          className="flex items-center gap-2 text-14"
+                          shallow
+                        >
+                          <span className="flex items-center gap-3">
+                            <Image
+                              className="w-10 h-10 rounded-md"
+                              src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${item?.logo}`}
+                              height={1000}
+                              width={1000}
+                              alt="Bangla"
+                            />
+                            {item?.name || " "}
+                          </span>
+                        </Link>
+                      </td>
+                      <td className="text-center border-r border-gray-200">
+                        {item?.sub_title?.length > 35
+                          ? item?.sub_title?.substring(0, 25) + "..."
+                          : item?.sub_title}
+                      </td>
+                      <td className="text-center border-r border-gray-200">
+                        {item?.type || ""}
+                      </td>
 
-                        <td className="text-center border-r border-gray-200">
-                          {item?.production_status || ""}
-                        </td>
-                        <td className="text-center border-r border-gray-200">
-                          <div className="flex items-center gap-2">
-                            {paidStatus?.free == 1 ? (
-                              <button className="bg-[#2F93DF] px-1 py-1 lg:px-3 lg:py-2 text-white  rounded ">
-                                Free
-                              </button>
-                            ) : (
-                              ""
-                            )}
+                      <td className="text-center border-r border-gray-200">
+                        {item?.production_status || ""}
+                      </td>
+                      <td className="text-center border-r border-gray-200">
+                        <div className="flex items-center gap-2">
+                          {paidStatus?.free == 1 ? (
+                            <button className="bg-[#2F93DF] px-1 py-1 lg:px-3 lg:py-2 text-white  rounded ">
+                              Free
+                            </button>
+                          ) : (
+                            ""
+                          )}
 
-                            {paidStatus?.pro == 1 && (
-                              <button className="bg-[#1AB17A] px-1 py-1 lg:px-3 lg:py-2 text-white  rounded ">
-                                Pro
-                              </button>
-                            )}
-                          </div>
-                        </td>
-                        <td className="text-center border-r border-gray-200">
-                          {
-                            item?.completion_status == 1 ? (
-                              <button
-                            onClick={() => UpdateServicePublishStatus(item?.id, item?.status == 1 ? 0 : 1)}
-                            className={`text-white px-4 py-2 rounded transition-all duration-300 w-[90%] ${
-                              item?.status == 1
+                          {paidStatus?.pro == 1 && (
+                            <button className="bg-[#1AB17A] px-1 py-1 lg:px-3 lg:py-2 text-white  rounded ">
+                              Pro
+                            </button>
+                          )}
+                        </div>
+                      </td>
+                      <td className="text-center border-r border-gray-200">
+                        {
+                          item?.completion_status == 1 ? (
+                            <button
+                              onClick={() => UpdateServicePublishStatus(item?.id, item?.status == 1 ? 0 : 1)}
+                              className={`text-white px-4 py-2 rounded transition-all duration-300 w-[90%] ${item?.status == 1
                                 ? "bg-green-700"
                                 : "bg-yellow-500"
-                            }`}
-                          >
-                            {item?.status == 1 ? "Publish" : "UnPublish"}
-                          </button>
-                            ) : (
-                              <button className="bg-white border border-red-500 text-red-500 px-4 py-2 w-[90%] rounded">
-                                Incomplete
-                              </button>
-                            )
-                          }
-                          
-                        </td>
-                        <td className="">
-                          <div className="w-full flex items-center justify-center gap-2">
-                            <>
-                              <Link
-                                href={{
-                                  pathname: `/admin/services/${item?.id}`,
-                                }}
-                                shallow
-                                className="px-2 py-1  bg-green-500 text-white active:scale-90 transition-all duration-400 rounded-md"
-                              >
-                                View
-                              </Link>
+                                }`}
+                            >
+                              {item?.status == 1 ? "Publish" : "UnPublish"}
+                            </button>
+                          ) : (
+                            <button className="bg-white border border-red-500 text-red-500 px-4 py-2 w-[90%] rounded">
+                              Incomplete
+                            </button>
+                          )
+                        }
 
-                              <Link
-                                href={{
-                                  pathname: `/admin/services/edit/${item?.id}`,
-                                }}
-                                className="px-2 py-1  bg-blue-500 text-white active:scale-90 transition-all duration-400 rounded-md"
-                              >
-                                Edit
-                              </Link>
-
-                              <button
-                                onClick={() => handleDelete(item?.id)}
-                                className="p-1  bg-red-500 text-white active:scale-90 transition-all duration-400 rounded-md"
-                              >
-                                <svg
-                                  className="w-4 h-4 fill-current"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 448 512"
+                      </td>
+                      <td className="">
+                        <div className="w-full flex items-center justify-center gap-2">
+                          <>
+                            {
+                              item?.completion_status == 1 && (
+                                <Link
+                                  href={{
+                                    pathname: `/admin/services/${item?.id}`,
+                                  }}
+                                  shallow
+                                  className="px-2 py-1  bg-green-500 text-white active:scale-90 transition-all duration-400 rounded-md"
                                 >
-                                  <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
-                                </svg>
-                              </button>
-                            </>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })
+                                  View
+                                </Link>
+                              )
+                            }
+
+
+                            <Link
+                              href={{
+                                pathname: `/admin/services/edit/${item?.id}`,
+                              }}
+                              className="px-2 py-1  bg-blue-500 text-white active:scale-90 transition-all duration-400 rounded-md"
+                            >
+                              { item?.completion_status == 1 ? "Edit" : "Update"}
+                            </Link>
+
+                            {
+                              item?.completion_status == 1 && (
+                                <button
+                                  onClick={() => handleDelete(item?.id)}
+                                  className="p-1  bg-red-500 text-white active:scale-90 transition-all duration-400 rounded-md"
+                                >
+                                  <svg
+                                    className="w-4 h-4 fill-current"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 448 512"
+                                  >
+                                    <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+                                  </svg>
+                                </button>
+                              )
+                            }
+                          </>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })
                 : ""}
             </tbody>
           </table>
