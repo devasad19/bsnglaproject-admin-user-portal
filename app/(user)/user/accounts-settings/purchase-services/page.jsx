dceb168075dab921dfb3119ceb711334d38492f9
sadaf <monower.mysoftheaven@gmail.com>
2024-11-05 10:33:22 +0600

    fixed user accounts settings -> purchase services -> image undefoned

--- app/(user)/user/accounts-settings/purchase-services/page.jsx
+++ app/(user)/user/accounts-settings/purchase-services/page.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import { getBoughtServices } from '../../../_api';
 import Image from "next/image";
+import { relative_image_path } from "@/helper";
 
 
 const Home = () => {
@@ -13,11 +14,14 @@ const Home = () => {
   useEffect(() => {
     setIsLoading(true);
     getBoughtServices(12)
-      .then((data) => (setServices(data?.data), setIsLoading(false)))
+      .then((data) => {
+        setServices(data?.data);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
- 
+
 
   return (
     <section>
@@ -50,69 +54,71 @@ const Home = () => {
                     </td>
                   </tr>
                 )}
-                {services?.map((item, index) => (
-                  <tr key={index}>
-                    <td className="px-2">
-                      <div className="flex items-center gap-2 text-14">
-                        <Image src={process.env.NEXT_PUBLIC_IMAGE_URL + (item?.service?.logo ?? '')} className="w-[5em] h-[5em]" width={1000} height={1000} alt="Bangla" />
-                        <span className="flex items-center gap-3">
-                          {item?.service?.name ?? ''}
-                        </span>
-                      </div>
-                    </td>
-                    <td className="text-center">
-                      <p dangerouslySetInnerHTML={{ __html: item?.service?.description?.length > 25 ? item?.service?.description?.substring(0, 25) + "..." : item?.service?.description }} />
-                    </td>
+                {services?.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td className="px-2">
+                        <div className="flex items-center gap-2 text-14">
+                          <Image src={item?.logo.length > 0 ? (process.env.NEXT_PUBLIC_IMAGE_URL + item?.logo) : relative_image_path('dummy_image1.jpg')} className="w-[5em] h-[5em]" width={1000} height={1000} alt="Bangla" />
+                          <span className="flex items-center gap-3">
+                            {item?.service?.name ?? ''}
+                          </span>
+                        </div>
+                      </td>
+                      <td className="text-center">
+                        <p dangerouslySetInnerHTML={{ __html: item?.description?.length > 25 ? item?.description?.substring(0, 25) + "..." : item?.service?.description }} />
+                      </td>
 
-                    <td
-                      className={`text-center ${item?.status === "1" && "text-green-500"
-                        } ${item?.status === "2" && "text-red-500"} ${item?.status === "3" && "text-gray-500"
-                        }`}
-                    >
-                      {item?.status === "1" && "Active"}
-                      {item?.status === "2" && "Inactive"}
-                      {item?.status === "3" && "Archeive"}
-                    </td>
-                    <td className="text-center">
-                      {new Date(item?.created_at).toLocaleDateString("en-GB", {
-                        day: "numeric",
-                        month: "long",
-                        year: "numeric",
-                      })}
-                    </td>
-                    <td className="text-center cursor-pointer">
+                      <td
+                        className={`text-center ${item?.status === "1" && "text-green-500"
+                          } ${item?.status === "2" && "text-red-500"} ${item?.status === "3" && "text-gray-500"
+                          }`}
+                      >
+                        {item?.status === "1" && "Active"}
+                        {item?.status === "2" && "Inactive"}
+                        {item?.status === "3" && "Archeive"}
+                      </td>
+                      <td className="text-center">
+                        {new Date(item?.created_at).toLocaleDateString("en-GB", {
+                          day: "numeric",
+                          month: "long",
+                          year: "numeric",
+                        })}
+                      </td>
+                      <td className="text-center cursor-pointer">
 
-                      <details className="dropdown">
-                        <summary className="btn m-1 h-auto min-h-1 border-none bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-500 hover:text-white">Options</summary>
-                        <ul className="menu dropdown-content bg-gray-100 rounded-box z-[1] w-40 p-1 shadow-md right-0">
-                          <li>
-                            <Link target="_blank" href={process.env.NEXT_PUBLIC_LOCAL_PORTAL_URL+'/services/'+item?.service_id}>
-                              Service Details
-                            </Link>
-                          </li>
-                          <li>
-                            <Link href={{
-                              pathname: `/user/services/${item?.service_id}`
-                            }}
-                            shallow
-                            >
-                              Upgrade Package
-                            </Link>
-                          </li>
-                          <li>
-                            <Link href={{
-                              pathname: `/user/accounts-settings/purchase-services/${item?.service_id}`
-                            }}
-                              shallow
-                            >
-                              Purchase Info
-                            </Link>
-                          </li>
-                        </ul>
-                      </details>
-                    </td>
-                  </tr>
-                ))}
+                        <details className="dropdown">
+                          <summary className="btn m-1 h-auto min-h-1 border-none bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-500 hover:text-white">Options</summary>
+                          <ul className="menu dropdown-content bg-gray-100 rounded-box z-[1] w-40 p-1 shadow-md right-0">
+                            <li>
+                              <Link target="_blank" href={process.env.NEXT_PUBLIC_LOCAL_PORTAL_URL + '/services/' + item?.service_id}>
+                                Service Details
+                              </Link>
+                            </li>
+                            <li>
+                              <Link href={{
+                                pathname: `/user/services/${item?.service_id}`
+                              }}
+                                shallow
+                              >
+                                Upgrade Package
+                              </Link>
+                            </li>
+                            <li>
+                              <Link href={{
+                                pathname: `/user/accounts-settings/purchase-services/${item?.service_id}`
+                              }}
+                                shallow
+                              >
+                                Purchase Info
+                              </Link>
+                            </li>
+                          </ul>
+                        </details>
+                      </td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
           </div>
