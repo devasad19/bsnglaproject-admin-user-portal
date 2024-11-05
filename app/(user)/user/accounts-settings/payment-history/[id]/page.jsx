31571ba26d321cebc9c4b486e086da76d9c9314a
sadaf <monower.mysoftheaven@gmail.com>
2024-11-05 11:24:50 +0600

    fixed the pdf design issue

--- app/(user)/user/accounts-settings/payment-history/[id]/page.jsx
+++ app/(user)/user/accounts-settings/payment-history/[id]/page.jsx
@@ -94,103 +94,107 @@ const Home = ({ params }) => {
                   </button>
                 </div>
               </div>
-              <div ref={divRef} className=" w-full flex-col justify-center items-center text-14">
-                <div
-                  id="invoice"
-                  className="w-2/3 bg-white p-6 shadow-lg rounded-md"
-                >
-                  <div className="pb-5">
-                    <div className="w-24 flex flex-col items-center gap-1 pb-3">
-                      <Image
-                        src={relative_image_path("logo.png")}
-                        className="h-10"
-                        width={1000}
-                        height={1000}
-                        alt="Bangla"
-                      />
-                      <p className="">Bangla Project</p>
-                    </div>
-                    <div className="grid grid-cols-2 pb-5">
-                      <div className="flex flex-col gap-1 ">
-                        <p>Address: ICT Tower,9th Floor, E-14/X, Sher-E-Bangla nagar, Dhaka-1207</p>
-                        <p>Phone: +88-02-55006880</p>
-                        <p>Email: pdeblict@bcc.net.bd</p>
-                        <p>www.bangla.com</p>
-                      </div>
-                      <div className="flex flex-col gap-1">
-                        <h3 className="text-20 text-red-500 font-bold">Invoice</h3>
-                        <p className="">Invoice No: {data?.invoice_no}</p>
-                        <p className="">Invoice Date: {data?.created_at}</p>
-                      </div>
-                    </div>
-                    <div className="w-full flex flex-col ">
-                      <p>Name: {data?.citizen_name}</p>
-                      <div className="grid grid-cols-2">
-                        <p>Phone: {data?.phone}</p>
-                        <p>Email: {data?.email}</p>
-                      </div>
-                      <p>Address: {data?.address}</p>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="w-full overflow-x-auto pb-5">
-                      <table className="w-full text-center border-collapse">
-                        <thead>
-                          <tr className="border border-gray-500 h-10">
-                            <th className="border border-gray-500">SI</th>
-                            <th className="border border-gray-500">Service</th>
-                            <th className="border border-gray-500">Plan</th>
-                            <th className="border border-gray-500">Price</th>
-                            <th className="border border-gray-500">Quantity</th>
-                            <th className="border border-gray-500">Amount</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr className="h-52">
-                            <td className="border border-gray-500">1</td>
-                            <td className="border border-gray-500">{data?.service_name}</td>
-                            <td className="border border-gray-500">{data?.feature_name}</td>
-                            <td className="border border-gray-500">{plan[0]?.price}</td>
-                            <td className="border border-gray-500">1</td>
-                            <td className="border border-gray-500">{plan[0]?.price}</td>
-                          </tr>
-                          <tr className="border border-gray-500 h-10">
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td className="border border-gray-500">Total</td>
-                            <td>{data?.total}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <div className="border-b border-gray-500 h-10 flex items-center">
-                        <p>Taka in Words: </p>
-                      </div>
-                    </div>
-                    <div className="flex justify-between">
-                      <div>
-                        <p>Terms and Conditions</p>
-                        <ul className="list-disc list-inside">
-                          <li>Lorem ipsum dolor sit amet</li>
-                          <li>Lorem ipsum dolor sit amet</li>
-                          <li>Lorem ipsum dolor sit amet</li>
-                        </ul>
-                      </div>
-                      <div className="pr-4 flex flex-col items-center">
+              <div className="flex justify-center">
+                <div  className="text-14 flex justify-center">
+                  <div
+                    ref={divRef}
+                    id="invoice"
+                    className="w-2/3 bg-white p-6 shadow-lg rounded-md"
+                  >
+                    <div className="pb-5">
+                      <div className="w-24 flex flex-col items-center gap-1 pb-3">
                         <Image
-                          src={relative_image_path("sign.jpg")}
-                          className="w-32 h-20"
+                          src={relative_image_path("logo.png")}
+                          className="h-10"
                           width={1000}
                           height={1000}
                           alt="Bangla"
                         />
-                        <p>Signature</p>
+                        <p className="">Bangla Project</p>
+                      </div>
+                      <div className="grid grid-cols-2 pb-5">
+                        <div className="flex flex-col gap-1 ">
+                          <p>Address: ICT Tower,9th Floor, E-14/X, Sher-E-Bangla nagar, Dhaka-1207</p>
+                          <p>Phone: +88-02-55006880</p>
+                          <p>Email: pdeblict@bcc.net.bd</p>
+                          <p>www.bangla.com</p>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                          <h3 className="text-20 text-red-500 font-bold">Invoice</h3>
+                          <p className="">Invoice No: {data?.invoice_no}</p>
+                          <p className="">Invoice Date: {data?.created_at}</p>
+                        </div>
+                      </div>
+                      <div className="w-full flex flex-col ">
+                        <p>Name: {data?.citizen_name}</p>
+                        <div className="grid grid-cols-2">
+                          <p>Phone: {data?.phone}</p>
+                          <p>Email: {data?.email}</p>
+                        </div>
+                        <p>Address: {data?.address}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="w-full overflow-x-auto pb-5">
+                        <table className="w-full text-center border-collapse">
+                          <thead>
+                            <tr className="border border-gray-500 h-10">
+                              <th className="border border-gray-500">SI</th>
+                              <th className="border border-gray-500">Service</th>
+                              <th className="border border-gray-500">Plan</th>
+                              <th className="border border-gray-500">Price</th>
+                              <th className="border border-gray-500">Quantity</th>
+                              <th className="border border-gray-500">Amount</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr className="h-52">
+                              <td className="border border-gray-500">1</td>
+                              <td className="border border-gray-500">{data?.service_name}</td>
+                              <td className="border border-gray-500">{data?.feature_name}</td>
+                              <td className="border border-gray-500">{plan[0]?.price}</td>
+                              <td className="border border-gray-500">1</td>
+                              <td className="border border-gray-500">{plan[0]?.price}</td>
+                            </tr>
+                            <tr className="border border-gray-500 h-10">
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td className="border border-gray-500">Total</td>
+                              <td>{data?.total}</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <div className="border-b border-gray-500 h-10 flex items-center">
+                          <p>Taka in Words: </p>
+                        </div>
+                      </div>
+                      <div className="flex justify-between">
+                        <div>
+                          <p>Terms and Conditions</p>
+                          <ul className="list-disc list-inside">
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                          </ul>
+                        </div>
+                        <div className="pr-4 flex flex-col items-center">
+                          <Image
+                            src={relative_image_path("sign.jpg")}
+                            className="w-32 h-20"
+                            width={1000}
+                            height={1000}
+                            alt="Bangla"
+                          />
+                          <p>Signature</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+
             </div>
           </section>
         )
