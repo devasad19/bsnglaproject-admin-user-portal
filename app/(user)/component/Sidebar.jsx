fcb2f618cd7a36cf0b5320b480e6fadd7c010ac2
sadaf <monower.mysoftheaven@gmail.com>
2024-12-04 12:36:47 +0600

    minor changes

--- app/(user)/component/Sidebar.jsx
+++ app/(user)/component/Sidebar.jsx
@@ -23,8 +23,8 @@ const Sidebar = () => {
   const HandleLogout = () => {
     document.cookie = "token=; path=/; domain=localhost; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
     document.cookie = "user=; path=/; domain=localhost; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    window.location.href='http://localhost:3000/signin';
-    // window.location.href=process.env.NEXT_PUBLIC_PORTAL_URL+'/signin';
+    // window.location.href='http://localhost:3000/signin';
+    window.location.href=process.env.NEXT_PUBLIC_PORTAL_URL+'/signin';
   }
 
   // console.log('user cookie: ',process.env.NEXT_PUBLIC_PORTAL_URL);
