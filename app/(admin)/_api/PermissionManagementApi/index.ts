dc1ae68f7a8b5263a63a19b1f45540b06e27c366
Prashoman1 <prashoman.mysoftheaven@gmail.com>
2024-12-12 13:13:07 +0600

    permission insert done

--- app/(admin)/_api/PermissionManagementApi/index.ts
+++ app/(admin)/_api/PermissionManagementApi/index.ts
@@ -1,34 +1,50 @@
 "use server";
 import axiosInstance from "@/lib/AxiosInstance";
 import { revalidateTag } from "next/cache";
+import { permission } from "process";
 // create user Type api
-
 
 // create single Parent Permission api
 export const createParentPermission = async (data: any) => {
-    try {
-        const response = await axiosInstance.post("/permission-parents", data, {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-          revalidateTag("get-parent-permissions");
-          return response.data;
-    } catch (error) {
-        throw new Error("Failed to Create Parent Permission data");
-    }
-}
+  try {
+    const response = await axiosInstance.post("/permission-parents", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    revalidateTag("get-parent-permissions");
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to Create Parent Permission data");
+  }
+};
 
+// create single permission api
 
+export const createSinglePermission = async (data: any) => {
+  try {
+    const response = await axiosInstance.post("/permissions", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    revalidateTag("get-permissions");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+
+    throw new Error("Failed to Create Permission data");
+  }
+};
 
 // get all Parent Permissions api
 export const getAllParentPermission = async () => {
   try {
     const fetchOption = {
-        next: {
-          tags: ["get-parent-permissions"],
-        },
-      };
+      next: {
+        tags: ["get-parent-permissions"],
+      },
+    };
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/permission-parents`,
       fetchOption
@@ -41,10 +57,30 @@ export const getAllParentPermission = async () => {
   } catch (error) {}
 };
 
-// get single user type
-export const getUserType = async (id:number) => {
+// get all permissions api
+export const getAllPermission = async () => {
   try {
-    const response = await axiosInstance.get(`/user-type/${id}`,{
+    const fetchOption = {
+      next: {
+        tags: ["get-permissions"],
+      },
+    };
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/permissions`,
+      fetchOption
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {}
+};
+
+// get single user type
+export const getUserType = async (id: number) => {
+  try {
+    const response = await axiosInstance.get(`/user-type/${id}`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -59,14 +95,13 @@ export const getUserType = async (id:number) => {
 
 export const manageUserTypeUpdate = async (data: any) => {
   try {
-    
     const response = await axiosInstance.post("/update/user-type", data, {
       headers: {
         "Content-Type": "application/json",
       },
     });
     // console.log(response);
-    
+
     revalidateTag("get-user-types");
 
     return response.data;
@@ -88,20 +123,21 @@ export const deleteUserType = async (id: number) => {
   } catch (error) {
     return error;
   }
-}
+};
 
 export const updateUserPermission = async (data: any) => {
   try {
-    const response = await axiosInstance.post("/update/user-type/permissions", data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axiosInstance.post(
+      "/update/user-type/permissions",
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     return error;
   }
-}
-
-
-
+};
--- app/(admin)/component/PermissionManagemntList/PermissionManagementList.tsx
+++ app/(admin)/component/PermissionManagemntList/PermissionManagementList.tsx
@@ -2,9 +2,12 @@
 import { modelClose, modelOpen } from "@/helper";
 
 import Modal from "@/app/_components/Modal/Modal";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import { createParentPermission } from "../../_api/PermissionManagementApi";
+import {
+  createParentPermission,
+  createSinglePermission,
+} from "../../_api/PermissionManagementApi";
 import { toast } from "react-toastify";
 
 import { CiEdit } from "react-icons/ci";
@@ -12,20 +15,29 @@ import { GrView } from "react-icons/gr";
 
 interface PermissionManagementListProps {
   parentPermissionData: { id: number; name: string }[];
+  permissionsData: any;
 }
 
 const PermissionManagementList = ({
   parentPermissionData,
+  permissionsData,
 }: PermissionManagementListProps) => {
   const addModal = useRef<any>(null);
   const updateModal = useRef<any>(null);
   const permissionModal = useRef<any>(null);
+  const permissionModalUpdate = useRef<any>(null);
+  const permissionUpdateModalForm = useRef<any>(null);
   // const protectedModal = useRef<any>(null);
-  console.log(parentPermissionData);
+  // console.log(parentPermissionData);
   const addModelForm = useRef<any>(null);
   const permissionModalForm = useRef<any>(null);
   // const updateModalForm = useRef<any>(null);
   // const protectedModalForm = useRef<any>(null);
+
+  const [validationError, setValidationError] = useState({
+    name: "",
+    display_name: "",
+  });
 
   const {
     register,
@@ -54,6 +66,74 @@ const PermissionManagementList = ({
       toast.error("Failed to Create Permission");
     }
   };
+
+  const handlePermissionSubmit = async (e: any) => {
+    e.preventDefault();
+    setValidationError({
+      name: "",
+      display_name: "",
+    });
+
+    const name = e.target.name.value;
+    const parent_id = e.target.parent_id.value;
+    const display_name = e.target.display_name.value;
+    if (name === "" || display_name === "") {
+      setValidationError({
+        name: "Name is Required",
+        display_name: "Display Name is Required",
+      });
+      return;
+    }
+
+    if (name == "") {
+      setValidationError({
+        name: "Name is Required",
+        display_name: "",
+      });
+      return;
+    }
+    if (display_name == "") {
+      setValidationError({
+        name: "",
+        display_name: "Display Name is Required",
+      });
+      return;
+    }
+
+    try {
+      const permissionData = {
+        name,
+        display_name,
+        parent_id,
+      };
+      console.log(permissionData);
+
+      const response = await createSinglePermission(permissionData);
+      console.log(response);
+
+      if (response.status) {
+        modelClose(permissionModal, permissionModalForm);
+        reset();
+        setValidationError({
+          name: "",
+          display_name: "",
+        });
+
+        toast.success("Permission Created Successfully");
+      }
+    } catch (error) {
+      toast.error("Failed to Create Permission");
+    }
+  };
+
+  const handlePermissionEdit = (id:string) =>{
+    if(id){
+      modelOpen(permissionUpdateModalForm);
+      console.log(id);
+      
+    }
+  }
+
   return (
     <>
       <div className="bg-white p-6">
@@ -88,120 +168,57 @@ const PermissionManagementList = ({
                 </tr>
               </thead>
               <tbody>
-                <tr className="h-16 border-b border-gray-300 hover:bg-gray-100">
-                  <td>1</td>
-                  <td>Demo Role</td>
-                  <td>Demo Role bangla</td>
-                  <td>Demo producer</td>
-                  <td>
-                    <span className="bg-green-500 text-white px-2 py-1 rounded-md">
-                      সক্রিয়
-                    </span>
-                  </td>
-                  <td className="flex items-center justify-center my-2">
-                    <div className="flex flex-row gap-2">
-                      <button className="px-2 py-1  bg-blue-500 text-white active:scale-90 transition-all duration-400 rounded-md">
-                        <CiEdit />
-                      </button>
-                      <button
-                        onClick={() => {
-                          confirm("Are you sure?");
-                        }}
-                        className="bg-red-500 text-white px-2 py-1 rounded-md"
+                {permissionsData?.length > 0 ? (
+                  <>
+                    {permissionsData?.map((item: any, index: number) => (
+                      <tr
+                        key={index}
+                        className="h-16 border-b border-gray-300 hover:bg-gray-100"
                       >
-                        <svg
-                          className="w-4 h-4 fill-current"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 448 512"
-                        >
-                          <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
-                        </svg>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="h-16 border-b border-gray-300 hover:bg-gray-100">
-                  <td>2</td>
-                  <td>Demo Role</td>
-                  <td>Demo Role bangla</td>
-                  <td>Demo producer</td>
-                  <td>
-                    <span className="bg-violet-500 text-white px-2 py-1 rounded-md">
-                      নিষ্ক্রিয়
-                    </span>
-                  </td>
-                  <td className="flex items-center justify-center my-2">
-                    <div className="flex flex-col gap-2">
-                      {/* <Modal
-                        id={"3"}
-                        trigger={"সংশোধন"}
-                        title={"অনুমতি সংশোধন"}
-                        subFunc={UpdateUser}
-                        subFuncTitle={"সংরক্ষণ করুন"}
-                      >
-                        <div className="pt-3 flex flex-col gap-3">
-                          <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
-                            <legend>
-                              <label
-                                htmlFor=""
-                                className="after:content-['_*'] after:text-red-400"
-                              >
-                                প্যারেন্ট অনুমতির নাম
-                              </label>
-                            </legend>
-                            <select
-                              name=""
-                              id=""
-                              className="w-full bg-white py-2"
+                        <td>{index + 1}</td>
+                        <td>{item?.name || ""}</td>
+                        <td>{item?.display_name || ""}</td>
+                        <td>{item?.parent?.name || ""}</td>
+                        <td>
+                          <span
+                            className={`px-2 py-1 rounded-md ${
+                              item.isActive
+                                ? "bg-green-500 text-white"
+                                : "bg-violet-500 text-white"
+                            }`}
+                          >
+                            {item.isActive ? "সক্রিয়" : "নিষ্ক্রিয়"}
+                          </span>
+                        </td>
+                        <td className="flex items-center justify-center my-2">
+                          <div className="flex flex-row gap-2">
+                            <button onClick={() => handlePermissionEdit(item?.id)} className="px-2 py-1 bg-blue-500 text-white active:scale-90 transition-all duration-400 rounded-md">
+                              <CiEdit />
+                            </button>
+                            <button
+                              onClick={() => confirm("Are you sure?")}
+                              className="bg-red-500 text-white px-2 py-1 rounded-md"
                             >
-                              <option value="1">parent 1</option>
-                              <option value="2">parent 2</option>
-                              <option value="3">parent 3</option>
-                            </select>
-                          </fieldset>
-                          <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
-                            <legend>
-                              <label
-                                htmlFor=""
-                                className="after:content-['_*'] after:text-red-400"
+                              <svg
+                                className="w-4 h-4 fill-current"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
                               >
-                                প্রদর্শনী নাম
-                              </label>
-                            </legend>
-                            <input
-                              type="text"
-                              className="w-full text-14 outline-none py-1"
-                              placeholder="অনুমতির প্রদর্শনী নাম লিখুন"
-                            />
-                          </fieldset>
-                          <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
-                            <legend>
-                              <label
-                                htmlFor=""
-                                className="after:content-['_*'] after:text-red-400"
-                              >
-                                অনুমতি নাম
-                              </label>
-                            </legend>
-                            <input
-                              type="text"
-                              className="w-full text-14 outline-none py-1"
-                              placeholder="Enter Permission Name"
-                            />
-                          </fieldset>
-                        </div>
-                      </Modal> */}
-                      <button
-                        onClick={() => {
-                          confirm("Are you sure?");
-                        }}
-                        className="bg-red-500 text-white px-2 py-1 rounded-md"
-                      >
-                        মুছে ফেলুন
-                      </button>
-                    </div>
-                  </td>
-                </tr>
+                                <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
+                              </svg>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </>
+                ) : (
+                  <tr>
+                    <td colSpan={6} className="text-center">
+                      Data Not Found
+                    </td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
@@ -266,8 +283,13 @@ const PermissionManagementList = ({
         modalRef={permissionModal}
         modalForm={permissionModalForm}
         title="প্যারেন্ট অনুমতির নাম তৈরি করুন"
+        setServiceValidation={setValidationError}
       >
-        <form className="pt-3" ref={permissionModalForm}>
+        <form
+          onSubmit={handlePermissionSubmit}
+          className="pt-3"
+          ref={permissionModalForm}
+        >
           <div className="pt-3 flex flex-col gap-3">
             <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
               <legend>
@@ -280,7 +302,7 @@ const PermissionManagementList = ({
               </legend>
               <select
                 disabled={parentPermissionData?.length > 0 ? false : true}
-                name=""
+                name="parent_id"
                 id=""
                 className="w-full bg-white py-2"
               >
@@ -302,10 +324,16 @@ const PermissionManagementList = ({
               </legend>
               <input
                 type="text"
+                name="display_name"
                 className="w-full text-14 outline-none py-1"
                 placeholder="অনুমতির প্রদর্শনী নাম লিখুন"
               />
             </fieldset>
+            {validationError.display_name && (
+              <span className="text-red-500">
+                {validationError.display_name}
+              </span>
+            )}
             <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
               <legend>
                 <label
@@ -317,15 +345,124 @@ const PermissionManagementList = ({
               </legend>
               <input
                 type="text"
+                name="name"
                 className="w-full text-14 outline-none py-1"
                 placeholder="Enter Permission Name"
               />
             </fieldset>
+            {validationError.name && (
+              <span className="text-red-500">{validationError.name}</span>
+            )}
           </div>
           <div className="flex justify-end gap-3 mt-7">
             <button
               type="button"
               onClick={() => {
+                setValidationError({
+                  name: "",
+                  display_name: "",
+                });
+                modelClose(permissionModal, permissionModalForm);
+              }}
+              className="bg-red-500 text-white px-4 py-2 rounded-md"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {/* permission edit modal start */}
+
+      <Modal
+        modalRef={permissionModalUpdate}
+        modalForm={permissionUpdateModalForm}
+        title="প্যারেন্ট অনুমতির নাম তৈরি করুন"
+        setServiceValidation={setValidationError}
+      >
+        <form
+          onSubmit={handlePermissionSubmit}
+          className="pt-3"
+          ref={permissionModalForm}
+        >
+          <div className="pt-3 flex flex-col gap-3">
+            <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
+              <legend>
+                <label
+                  htmlFor=""
+                  className="after:content-['_*'] after:text-red-400"
+                >
+                  প্যারেন্ট অনুমতির নাম
+                </label>
+              </legend>
+              <select
+                disabled={parentPermissionData?.length > 0 ? false : true}
+                name="parent_id"
+                id=""
+                className="w-full bg-white py-2"
+              >
+                {parentPermissionData?.map((item: any, index: number) => (
+                  <option key={index} value={item.id}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+            </fieldset>
+            <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
+              <legend>
+                <label
+                  htmlFor=""
+                  className="after:content-['_*'] after:text-red-400"
+                >
+                  প্রদর্শনী নাম
+                </label>
+              </legend>
+              <input
+                type="text"
+                name="display_name"
+                className="w-full text-14 outline-none py-1"
+                placeholder="অনুমতির প্রদর্শনী নাম লিখুন"
+              />
+            </fieldset>
+            {validationError.display_name && (
+              <span className="text-red-500">
+                {validationError.display_name}
+              </span>
+            )}
+            <fieldset className="flex flex-col border border-gray-300 rounded-md px-2">
+              <legend>
+                <label
+                  htmlFor=""
+                  className="after:content-['_*'] after:text-red-400"
+                >
+                  অনুমতি নাম
+                </label>
+              </legend>
+              <input
+                type="text"
+                name="name"
+                className="w-full text-14 outline-none py-1"
+                placeholder="Enter Permission Name"
+              />
+            </fieldset>
+            {validationError.name && (
+              <span className="text-red-500">{validationError.name}</span>
+            )}
+          </div>
+          <div className="flex justify-end gap-3 mt-7">
+            <button
+              type="button"
+              onClick={() => {
+                setValidationError({
+                  name: "",
+                  display_name: "",
+                });
                 modelClose(permissionModal, permissionModalForm);
               }}
               className="bg-red-500 text-white px-4 py-2 rounded-md"
